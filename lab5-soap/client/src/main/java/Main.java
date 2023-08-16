7d970ac70de998f27c32e6fdd99f3edb4f074ee4
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-08-15 21:06:52 -0500

    feat(lab5): Use `Song` object's methods in the client side

--- lab5-soap/client/src/main/java/Main.java
+++ lab5-soap/client/src/main/java/Main.java
@@ -9,10 +9,23 @@ public class Main {
     public static void main(String[] args) {
         MemorySongRepositoryService songsRepositoryService = new MemorySongRepositoryService();
         SongRepository songsRepository = songsRepositoryService.getMemorySongRepositoryPort();
-        List<Song> filteredByGenreSongs = songsRepository.getByGenre("Rap");
 
+        List<Song> filteredByGenreSongs = songsRepository.getByGenre("Rap");
+        System.out.println("Songs filtered by genre:");
         for(Song s: filteredByGenreSongs.stream().collect(Collectors.toList())) {
-            System.out.println(s);
+            System.out.println(s.getTitle());
+        }
+
+        List<Song> filteredByYearSongs = songsRepository.getByYear(2007);
+        System.out.println("\nSongs filtered by year:");
+        for(Song s: filteredByYearSongs.stream().collect(Collectors.toList())) {
+            System.out.println(s.getTitle());
+        }
+
+        List<Song> filteredByNameSongs = songsRepository.getByName("Song 1");
+        System.out.println("\nSongs filtered by name:");
+        for(Song s: filteredByNameSongs.stream().collect(Collectors.toList())) {
+            System.out.println(s.getTitle());
         }
     }
 }
